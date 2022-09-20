f2fec63158be32a8f558296a18bc4aaac876d973
MinxSu <sian415@gmail.com>
2022-09-20 08:02:09 +0000

    v1

---
+++ WCFClient/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using WCFClient.ServiceReference1;
+
+namespace WCFClient
+{
+    class Project
+    {
+        static void Main()
+        {
+            ServiceClient client = new ServiceClient();
+
+            // Use the 'client' variable to call operations on the service.
+            string response = client.GetData(123);
+            Console.Write(response);
+            // Always close the client.
+            client.Close();
+        }
+    }
+}
