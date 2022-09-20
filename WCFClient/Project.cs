962ce66a26f6d787a23b1f8023edc940c2ad5b6c
MinxSu <sian415@gmail.com>
2022-09-20 09:25:53 +0000

    set Client

--- WCFClient/Project.cs
+++ WCFClient/Project.cs
@@ -8,12 +8,33 @@ namespace WCFClient
         static void Main()
         {
             ServiceClient client = new ServiceClient();
-
-            // Use the 'client' variable to call operations on the service.
-            string response = client.GetData(123);
-            Console.Write(response);
+            Console.WriteLine("Enter any number you want...");
             // Always close the client.
+            int data = ReadKey();
+            // Use the 'client' variable to call operations on the service.
+            string response = client.GetData(data);
+            Console.WriteLine(response);
+            Console.Write("Press any key to stop...");
+            // Always close the client.
+            Console.ReadKey();
             client.Close();
+        }
+        static int ReadKey()
+        {
+            string data = Console.ReadLine();
+            if (string.IsNullOrEmpty(data))
+            {
+                Console.WriteLine("Data Required");
+                return ReadKey();
+            }
+            int number;
+            bool success = int.TryParse(data, out number);
+            if (!success)
+            {
+                Console.WriteLine($"[N.U.M.B.E.R]");
+                return ReadKey();
+            }
+            return number;
         }
     }
 }
